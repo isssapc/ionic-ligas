56960424acccf8b733d419d995dc298c724e5ac4
Ramiro Arechar <arechar@local.com>
2017-12-19 16:11:00 -0600

    ok

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,7 +1,7 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, PopoverController } from 'ionic-angular';
 
-
+import { Slides } from 'ionic-angular';
 import { PopoverPage } from '../about-popover/about-popover';
 
 @IonicPage()
@@ -12,13 +12,46 @@ import { PopoverPage } from '../about-popover/about-popover';
 export class AboutPage {
   conferenceDate = '2047-05-17';
 
-  constructor(public navCtrl: NavController,public popoverCtrl: PopoverController) { }
+  @ViewChild(Slides) slides: Slides;
+
+  constructor(public navCtrl: NavController, public popoverCtrl: PopoverController) { }
+
+  ngAfterViewInit() {
+    //this.slides.freeMode = true;
+    this.slides.centeredSlides = true;
+  }
 
   presentPopover(event: Event) {
     let popover = this.popoverCtrl.create(PopoverPage);
     popover.present({ ev: event });
   }
   gotoPartido() {
-      this.navCtrl.push("PartidoPage");
+    this.navCtrl.push("PartidoPage");
+  }
+
+  slideChanged() {
+    console.log("slide change");
+    let currentIndex = this.slides.getActiveIndex();
+    //console.log('Current index is', currentIndex);
+    //console.log("slides", this.slides);
+
+    //console.log("length", this.slides.length());
+  }
+
+  getIndex(slide) {
+
+    console.log("prop de slide", slide);
+
+    let currentIndex = this.slides.getActiveIndex();
+    console.log('Current index is', currentIndex);
+  }
+
+  slideTap(event) {
+    console.log("slide tap/click");
+    //console.log("slide tap", event);
+    let currentIndex = this.slides.getActiveIndex();
+    if (event.clickedIndex != currentIndex) {
+      this.slides.slideTo(event.clickedIndex);
+    }
   }
 }
